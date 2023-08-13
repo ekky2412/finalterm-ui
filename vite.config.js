2318af10400d94e492fcd2b76b69d95f04313139
ekky2412 <ekky.raharjo@gmail.com>
2023-08-13 20:18:08 +0700

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -8,6 +8,6 @@ export default defineConfig({
     host: true,
   },
   define: {
-    "process.env.VITE_API_URL": `"${process.env.VITE_API_URL}"`,
+    "process.env.VITE_API_URL": `"${import.meta.env.VITE_API_URL}"`,
   },
 });
