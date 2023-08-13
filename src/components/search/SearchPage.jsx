d7759cf309c6eeba9a9b18d95d289213599f1acf
ekky2412 <ekky.raharjo@gmail.com>
2023-08-14 00:16:43 +0700

    add search

--- src/components/search/SearchPage.jsx
+++ src/components/search/SearchPage.jsx
@@ -10,19 +10,19 @@ import { getVideoByTitle } from "../../services/ApiServices";
 import VideoCard from "../main/VideoCard";
 
 const SearchPage = () => {
-  const [search, setSearch] = useState({});
-  const [videos, setVideos] = useState({});
+  const [search, setSearch] = useState([]);
+  const [videos, setVideos] = useState([]);
   useEffect(() => {
     getVideoByTitle(search).then((videos) => {
       setVideos(videos);
     });
   }, [search]);
 
-  //   const VideoRow = (video, index) => {
-  //     return <VideoCard key={index} video={video}></VideoCard>;
-  //   };
+  const VideoRow = (video, index) => {
+    return <VideoCard key={index} video={video}></VideoCard>;
+  };
 
-  //   const videosCard = videos.map((video, index) => VideoRow(video, index));
+  const videosCard = videos.map((video, index) => VideoRow(video, index));
 
   return (
     <div>
@@ -36,13 +36,13 @@ const SearchPage = () => {
         ></Input>
       </InputGroup>
       <div>
-        {/* <SimpleGrid
+        <SimpleGrid
           spacing={5}
           columns={3}
           templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
         >
           {videosCard}
-        </SimpleGrid> */}
+        </SimpleGrid>
       </div>
     </div>
   );
