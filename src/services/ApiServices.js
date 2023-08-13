273fa46c0b9bcb453599f2779cccee4799391f47
ekky2412 <ekky.raharjo@gmail.com>
2023-08-13 20:32:59 +0700

    edit

--- src/services/ApiServices.js
+++ src/services/ApiServices.js
@@ -1,30 +1,41 @@
 export async function getAllVideos() {
-  const response = await fetch(`${VITE_API_URL}/api/v1/videos/thumbnail`);
+  const response = await fetch(
+    `${process.env.VITE_API_URL}/api/v1/videos/thumbnail`
+  );
   console.log(response);
   return await response.json();
 }
 
 export async function getVideo(videoId) {
-  const response = await fetch(`${VITE_API_URL}/api/v1/video/${videoId}`);
+  const response = await fetch(
+    `${process.env.VITE_API_URL}/api/v1/video/${videoId}`
+  );
   return await response.json();
 }
 
 export async function postComment(data, videoID) {
   console.log(data);
-  const response = await fetch(`${VITE_API_URL}/api/v1/comments/${videoID}`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
+  const response = await fetch(
+    `${process.env.VITE_API_URL}/api/v1/comments/${videoID}`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }
+  );
   return await response.json();
 }
 
 export async function getComments(videoId) {
-  const response = await fetch(`${VITE_API_URL}/api/v1/comments/${videoId}`);
+  const response = await fetch(
+    `${process.env.VITE_API_URL}/api/v1/comments/${videoId}`
+  );
   return await response.json();
 }
 
 export async function getProducts(videoId) {
-  const response = await fetch(`${VITE_API_URL}/api/v1/products/${videoId}`);
+  const response = await fetch(
+    `${process.env.VITE_API_URL}/api/v1/products/${videoId}`
+  );
   return await response.json();
 }
