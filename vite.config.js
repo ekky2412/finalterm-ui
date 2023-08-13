128418b352aa924a7f361f4ae2c755cc88ded1fd
ekky2412 <ekky.raharjo@gmail.com>
2023-08-13 20:39:42 +0700

    edit

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import react from "@vitejs/plugin-react";
 export default ({ mode }) => {
   dotenv.config({ path: `./.env.${mode}` });
   // now you can access config with process.env.{configName}
-  console.log(process.env);
-  console.log(import.meta.env);
 
   return defineConfig({
     plugins: [react()],
